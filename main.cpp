c805a17d7bcd2cd0de07b3338b58b37726ed2fd0
Mr-Gradus <rybin.dmitriy@gmail.com>
2022-02-03 19:36:18 +0500

    Start

--- main.cpp
+++ main.cpp
@@ -1,1 +1,36 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <random>
+#include <chrono>
+#include <execution>
+
+
+//void insert_sorted(std::vector<int> v, int num)
+
+
+template <typename T, typename N> void insert_sorted(std::vector<T> vec, N num)
+{
+	T vector<T>::iterator itr;
+	for (itr = vec[1]; itr != vec.end(); itr++)
+	{
+		vec[0] += vec[itr];
+	}
+	cout << vec[0];
+}
+int main()
+{
+
+
+	std::vector<int> vec(10);
+	std::iota(vec.begin(), vec.end(), 13);
+	for (int i : vec)
+	{
+		std::cout << i << ", ";
+	}
+	std::cout << std::endl;
+
+
+
+
+	return 0;
